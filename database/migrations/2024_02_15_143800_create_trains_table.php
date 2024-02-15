fa3bcfa4cf2f955ed57fbca52a5aa16c56ade82d
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-15 16:12:37 +0100

    Defined table structure

--- database/migrations/2024_02_15_143800_create_trains_table.php
+++ database/migrations/2024_02_15_143800_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('carriages_number')->nullable();
+            $table->boolean('in_time');
+            $table->boolean('deleted');
             $table->timestamps();
         });
     }
