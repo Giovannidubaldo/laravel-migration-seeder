2d35058faa1374df4ed576954378dc615274198e
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-02-15 17:39:49 +0100

    Added Column

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Model\Train;
+use Faker\Generator as Faker;
+use App\Models\Train;
+use Faker\Provider\en_US\Address;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,8 +15,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+            $new_train = new Train();
+            $new_train->company = $faker->company();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->date('Y_m_d');
+            $new_train->arrival_time = $faker->datetime();
+            $new_train->departure_date = $faker->dateTime();
+            $new_train->train_code = $faker->numberBetween(100, 1000);
+            $new_train->carriages_number = $faker->numberBetween(2, 15);
+            $new_train->in_time = $faker->boolean();
+            $new_train->deleted = $faker->boolean();
+            $new_train->save();
+        }
     }
 }
